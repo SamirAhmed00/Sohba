f4d6ccebd4283fd0a0812b07cd8422cdafff0d81
basmf <basmf@DESKTOP-I9HQ4DE>
2026-01-10 23:16:23 +0200

    Add project files.

---
+++ Sohba/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Sohba.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
