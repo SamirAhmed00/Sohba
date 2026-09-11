44e448f58d12807836289c9d4fbd0da0663eae60
Samir Ahmed <basm.fathy159@gmail.com>
2026-09-11 23:44:18 +0300

    feat(dashboard): extend user response DTO with role and promotion metadata

--- Sohba.Application/DTOs/UserAggregate/UserResponseDto.cs
+++ Sohba.Application/DTOs/UserAggregate/UserResponseDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Sohba.Domain.Enums;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -16,5 +17,13 @@ namespace Sohba.Application.DTOs.UserAggregate
         public bool IsBlocked { get; set; }
         public bool IsPrivateAccount { get; set; }
         public bool IsActive { get; set; }
+
+        public int FriendsCount { get; set; }
+        public int PostsCount { get; set; }
+
+        public UserRole Role { get; set; } = UserRole.User;
+        public Guid? PromotedByAdminUserId { get; set; }
+        public string? PromotedByAdminName { get; set; }
+        public bool CanCurrentAdminManageRole { get; set; }
     }
 }
